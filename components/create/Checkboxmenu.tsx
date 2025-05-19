72b56c276507877787199dc3342249f567012c7c
yshak <Vyshakn29@gmail.com>
2025-05-19 16:55:58 +0530

    global variable modification for input

--- components/create/Checkboxmenu.tsx
+++ components/create/Checkboxmenu.tsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import { Checkbox } from '../ui/checkbox'
-
+import { Copy, Trash2 } from 'lucide-react'
+import { useFormStore } from '@/lib/useFormData'
+import {BlockType} from '@/types/formTypes' 
 
 interface CheckboxmenuProps {
+    id:number,
     label: string,
+    checkBoxref: React.RefObject<HTMLInputElement | null>
 }
 
 
 
-function Checkboxmenu({ label }: CheckboxmenuProps) {
+
+function Checkboxmenu({ label,id }: CheckboxmenuProps) {
+    const { removeFormData, formData} = useFormStore();
+
+
     return (
-        <div className='flex gap-x-2 items-center justify-center w-fit'>
-            <Checkbox/>
-            <input type="text" placeholder={label} className='text-sm w-screen border-none outline-none font-semibold text-neutral-900' />  
+        <div className='flex items-center justify-start w-fit'>
+            <div className='flex gap-x-2 items-center justify-center w-fit'>
+                <Checkbox />
+                <input  type="text" placeholder={label} className='text-sm w-sm border-none outline-none font-semibold text-neutral-900' />
+            </div>
+            <div className='flex gap-x-2 items-center justify-center w-fit'>    
+                <div className='flex items-center cursor-pointer hover:bg-neutral-100 rounded justify-center w-fit h-fit p-1'>
+                    <Copy size={18} className='text-neutral-400' />
+                </div>
+                <div  className='flex items-center cursor-pointer hover:bg-neutral-100 rounded justify-center w-fit h-fit p-1'>
+                    <Trash2 size={18} className='text-neutral-400'/> 
+                </div> 
+            </div>
         </div>
     )
 }
--- lib/useFormData.ts
+++ lib/useFormData.ts
@@ -6,18 +6,20 @@ interface store {
     formData:FormStore|[],
     removeFormData:(block:any)=>void,
     addFormData:(block:any)=>void,
-    updateFormData:(id:number,entry:string)=>void,
+    updateLabel:(id:number,entry:string)=>void,
+    removecheckBox:(checkBoxid)=>void
 }
 
 
 
 const useFormStore = create<store>((set) => ({
   formData:[{id:1, type:BlockType.ShortQuestion, label:"how is the josh",required:false},{id:2, type:BlockType.LongQuestion, label:"how is the josh",required:false}
-    ,{id:3, type:BlockType.CheckBoxes, label:"how is the josh",required:false,options:[{checked:false,label:"option 1"},{checked:false,label:"option 2"}]}
+    ,{id:3, type:BlockType.CheckBoxes, label:"how is the josh",required:false,options:[{checked:false,label:"option 1",id:1},{checked:false,label:"option 2",id:2}]}
   ],
   addFormData: (block) => set((state) => ({ formData: [...state.formData, block] })),
   removeFormData:(id:number)=> set((state)=>({formData:state.formData.filter((item)=>item.id!==id)})),
-  updateFormData:(id:number,entry:string)=>set((state)=>({formData:state.formData.map((item)=>item.id===id?{...item,label:entry}:item)}))
+  updateLabel:(id:number,entry:string)=>set((state)=>({formData:state.formData.map((item)=>item.id===id?{...item,label:entry}:item)})),
+  removecheckBox:
 }));
 
 export { useFormStore};

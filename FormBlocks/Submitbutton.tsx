82d86a5122deac179532b0679dec73e454f68a60
yshak <Vyshakn29@gmail.com>
2025-06-09 18:51:20 +0530

    btn

---
+++ FormBlocks/Submitbutton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/export'
+
+
+function Submitbutton() {
+  return (
+    <div className='flex flex-col items-start justify-center'>
+      <Button>{"submit"}</Button>
+    </div>
+  )
+}
+
+export default Submitbutton
